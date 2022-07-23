793469f967996d839c7af5834eeb945e82af9356
Warren R. Liske <61427748+liskewa@users.noreply.github.com>
2022-07-23 07:51:41 -0700

    Update HelloWorld.java

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -4,6 +4,5 @@ public class HelloWorld {
   public static void main(String[] args) {
     Greeter greeter = new Greeter();
     System.out.println(greeter.sayHello());
-BAD LINE TO TRIGGER ERROR
   }
 }
